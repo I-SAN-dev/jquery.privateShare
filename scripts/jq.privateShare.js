084dfcbc1d4107cdab2c414e2336e1797daac31b
Sebastian Antosch <s.antosch@i-san.de>
2016-04-10 20:16:15 +0200

    add some ligc

--- scripts/jq.privateShare.js
+++ scripts/jq.privateShare.js
@@ -19,6 +19,9 @@
     }
 
     var
+        /**
+         * Holds some constants
+         */
         constants = {
             FB: {
                 POPUP: {
@@ -37,11 +40,129 @@
                     WIDTH: 695,
                     HEIGHT: 254
                 }
+            },
+            POPUPSETTINGS: {
+                directories: 0,
+                fullscreen: 0,
+                location: 0,
+                menubar: 0,
+                status: 0,
+                titlebar: 0,
+                toolbar: 0
             }
         },
 
-    // Save all functions in here
-        publicFunctions = {};
+        /**
+         * Holds the functions to generate the sharelinks
+         * @type {{}}
+         */
+        getShareLink = {
+            /**
+             * Generates a facebook share link
+             * @param {string} url
+             * @returns {string}
+             */
+            FB: function (url) {
+                return 'todo';
+            },
+            /**
+             * Generates a Google+ Share Link
+             * @param {string} url
+             * @returns {string}
+             */
+            GP: function (url) {
+                return 'todo';
+            },
+            /**
+             * Generates a Twitter tweet link with an optional message
+             * @param {string} url
+             * @param {string} message
+             * @returns {string}
+             */
+            TW: function (url, message) {
+                return 'todo';
+            }
+        },
+
+        /**
+         * Opens a popup for a share-service with specific dimensions.
+         * Opens a new tab on mobile view instead
+         * @param {string} service - FB (Facebook), GP (Google+), TW (Twitter)
+         * @param {string} link - the link to be called containing all share parameters
+         * @return {Window} - the opened window instance
+         */
+        openPopup = function (service, link) {
+            var width = constants[service].POPUP.WIDTH,
+                height = constants[service].POPUP.HEIGHT,
+                $win = $(window),
+                wwidth = $win.width(),
+                wheight = $win.height,
+                settings = constants.POPUPSETTINGS,
+                settingsstring = '',
+                win;
+
+            // Open in a new tab if the popup would exceed the parent windows dimensions
+            if (width > wwidth || height > wheight) {
+                win = window.open(link, '_blank');
+                win.focus();
+                return win;
+            }
+
+            // Otherwise open as a correctly sized and positioned popup
+            settings.width = width;
+            settings.height = height;
+            settings.top = (wheight - height) / 2;
+            settings.left = (wwidth - width) / 2;
+
+            $.each(function (key, value) {
+                settingsstring += (key + "=" + value + ",");
+            });
+            win = window.open(link, "privateSharePopup", settingsstring);
+            win.focus();
+            return win;
+        },
+
+        /**
+         * Attaches the sharelink functionality to a supplied link element
+         * @param {jQuery} $link - a jQuery link object
+         * @param {string} service - FB (Facebook), GP (Google+), TW (Twitter)
+         * @param {string} url - the link that shall be shared
+         * @param {string} message - an additional message, only for tweet links
+         * @return {jQuery}
+         */
+        attachSharelink = function ($link, service, url, message) {
+
+            // if no url is supplied, use the current one
+            if (!url) {
+                url = window.location.href;
+            }
+
+            // get the share link
+            var href = getSharelink[service](url, message),
+                alreadyClicked = false;
+
+            // set the sharelink
+            $link.attr('href', href);
+            $link.on('click touchstart', function (e) {
+
+                // Only fire once every 500ms preventing ghostclicks,
+                // this allows us to listen for touchstart as well
+                if (!alreadyClicked) {
+                    alreadyClicked = true;
+                    setTimeout(function () { alreadyClicked = false; }, 500);
+
+                    // Open the sharelink
+                    openPopup(service, href);
+                }
+
+                e.preventDefault();
+            });
+        },
+
+        // Save all functions in here
+        publicFunctions = {
+
+        };
 
     // Register the jQuery plugin
     $.fn.privateShare = function (action, options) {
